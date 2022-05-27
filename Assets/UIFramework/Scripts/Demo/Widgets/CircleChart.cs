06f94454797404e1ceb4ecd68ab47c9ae44f4d3b
yinczar <410781942@qq.com>
2022-05-27 09:48:07 +0800

    Update

---
+++ Assets/UIFramework/Scripts/Demo/Widgets/CircleChart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class CircleChart : MonoBehaviour
+{
+
+    public Image image_B;
+    public Image image_F;
+
+
+
+
+    public void ToValue( float  value )
+    { 
+    
+   // DOTween.To( )
+    
+    }
+
+
+
+}
